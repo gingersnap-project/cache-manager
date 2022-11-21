17ed31b4bd2a4fecf4649d8f37da4bc514971726
Ryan Emerson <remerson@redhat.com>
2022-11-21 08:49:26 +0000

    Native CacheManager
    * Added Embedded HotRod Server
    * Quarkus Extension added for native compilation support
    * Add Pull Request GH action
    
    Security implementation required

---
+++ manager/src/main/java/io/gingersnap/HotRodServer.java
@@ -0,0 +1,52 @@
+package io.gingersnap;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.server.core.admin.AdminOperationsHandler;
+import org.infinispan.server.core.admin.embeddedserver.CacheCreateTask;
+import org.infinispan.server.core.admin.embeddedserver.CacheGetOrCreateTask;
+import org.infinispan.server.core.admin.embeddedserver.CacheNamesTask;
+import org.infinispan.server.core.admin.embeddedserver.CacheRemoveTask;
+import org.infinispan.server.hotrod.configuration.HotRodServerConfiguration;
+import org.infinispan.server.hotrod.configuration.HotRodServerConfigurationBuilder;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+
+@Singleton
+public class HotRodServer {
+
+   @Inject
+   EmbeddedCacheManager cacheManager;
+
+   org.infinispan.server.hotrod.HotRodServer server;
+
+   void start(@Observes StartupEvent ignore) {
+      HotRodServerConfiguration build = new HotRodServerConfigurationBuilder()
+            .adminOperationsHandler(new EmbeddedServerAdminOperationHandler())
+            .build();
+      server = new org.infinispan.server.hotrod.HotRodServer();
+      server.start(build, cacheManager);
+   }
+
+   void stop(@Observes ShutdownEvent ignore) {
+      if (server != null) {
+         server.stop();
+      }
+   }
+
+   static class EmbeddedServerAdminOperationHandler extends AdminOperationsHandler {
+
+      public EmbeddedServerAdminOperationHandler() {
+         super(
+               new CacheCreateTask(),
+               new CacheGetOrCreateTask(),
+               new CacheNamesTask(),
+               new CacheRemoveTask()
+         );
+      }
+   }
+}
---
+++ manager/src/main/java/io/gingersnap/RuleResource.java
@@ -0,0 +1,38 @@
+package io.gingersnap;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.infinispan.Cache;
+import org.infinispan.manager.EmbeddedCacheManager;
+
+@Path("/rules")
+public class RuleResource {
+
+   @Inject
+   EmbeddedCacheManager cacheManager;
+
+   @GET
+   @Path("/{rule}/{key}")
+   @Produces("application/json")
+   public String get(String rule, String key) {
+      key = String.format("%s:%s", rule, key);
+      return cache(rule).get(key);
+   }
+
+   @GET
+   @Path("/{rule}")
+   public Collection<String> getAllKeys(String rule) {
+      return cache(rule).keySet().stream().map(s -> s.replaceFirst("^" + rule + ":", "")).collect(Collectors.toList());
+   }
+
+   private Cache<String, String> cache(String rule) {
+      // TODO use rule parameter when Cache per Rule created by DB Syncer
+      return cacheManager.getCache("debezium-cache");
+   }
+}
