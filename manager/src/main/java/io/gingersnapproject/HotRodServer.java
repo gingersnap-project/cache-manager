5fdc3a424366cc5a407eafd3c353ab819c3ba9c9
Ryan Emerson <remerson@redhat.com>
2022-12-08 17:30:50 +0000

    Make HotRod server configurable via properties

--- manager/src/main/java/io/gingersnapproject/HotRodServer.java
+++ manager/src/main/java/io/gingersnapproject/HotRodServer.java
@@ -3,12 +3,14 @@ package io.gingersnapproject;
 import java.net.InetSocketAddress;
 
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import org.infinispan.commons.logging.LogFactory;
 import org.infinispan.server.core.logging.Log;
 import org.infinispan.server.core.transport.NettyTransport;
 
+import io.gingersnapproject.configuration.Configuration;
 import io.gingersnapproject.hotrod.CommandProcessor;
 import io.gingersnapproject.hotrod.GingersnapDecoder;
 import io.gingersnapproject.hotrod.GingersnapServerConfiguration;
@@ -22,10 +24,14 @@ import io.quarkus.runtime.StartupEvent;
 public class HotRodServer {
    static private final Log log = LogFactory.getLog(HotRodServer.class, Log.class);
 
+   @Inject
+   Configuration config;
+
    NettyTransport nettyTransport;
 
    void start(@Observes StartupEvent ignore, Caches maps) {
-      GingersnapServerConfiguration configuration = new GingersnapServerConfigurationBuilder(11222)
+      GingersnapServerConfiguration configuration = new GingersnapServerConfigurationBuilder(config.hotrod().port())
+            .host(config.hotrod().host())
             .build();
       log.infof("Starting Netty transport for %s on %s:%s", configuration.name(), configuration.host(), configuration.port());
       InetSocketAddress address = new InetSocketAddress(configuration.host(), configuration.port());
