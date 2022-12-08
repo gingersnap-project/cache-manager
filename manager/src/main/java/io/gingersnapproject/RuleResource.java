1da043a62fb1be21be48057be4984f3667991859
Ryan Emerson <remerson@redhat.com>
2022-12-08 12:48:29 +0000

    Add OpenAPI extension

--- manager/src/main/java/io/gingersnapproject/RuleResource.java
+++ manager/src/main/java/io/gingersnapproject/RuleResource.java
@@ -6,6 +6,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+
 import io.smallrye.mutiny.Multi;
 
 @Path("/rules")
@@ -15,6 +17,7 @@ public class RuleResource {
    Caches maps;
 
    @GET
+   @Operation(summary = "Retrieve a cache entry associated with the provided rule and key")
    @Path("/{rule}/{key}")
    @Produces(MediaType.APPLICATION_JSON)
    public String get(String rule, String key) {
@@ -23,6 +26,7 @@ public class RuleResource {
    }
 
    @GET
+   @Operation(summary = "Retrieve all cached entries associated with the provided rule")
    @Path("/{rule}")
    @Produces(MediaType.APPLICATION_JSON)
    public Multi<String> getAllKeys(String rule) {
