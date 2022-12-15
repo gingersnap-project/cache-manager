56d9993fe76fd72145d8b1366bf4b02ce2fd1def
William Burns <mudokonman@gmail.com>
2022-12-15 11:58:50 -0800

    Only perform indexing operations on rules that have it enabled

--- manager/src/main/java/io/gingersnapproject/search/QueryHandler.java
+++ manager/src/main/java/io/gingersnapproject/search/QueryHandler.java
@@ -4,6 +4,8 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import io.gingersnapproject.Caches;
+import io.gingersnapproject.configuration.Configuration;
+import io.gingersnapproject.configuration.Rule;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
@@ -11,9 +13,26 @@ import io.smallrye.mutiny.Uni;
 public class QueryHandler {
    @Inject
    Caches caches;
-
    @Inject
    SearchBackend searchBackend;
+   @Inject
+   Configuration configuration;
+
+   public Uni<String> put(String indexName, String documentId, String jsonString) {
+      Rule rule = configuration.rules().get(indexName);
+      if (rule == null || !rule.queryEnabled()) {
+         return Uni.createFrom().nullItem();
+      }
+      return searchBackend.put(indexName, documentId, jsonString);
+   }
+
+   public Uni<String> remove(String indexName, String documentId) {
+      Rule rule = configuration.rules().get(indexName);
+      if (rule == null || !rule.queryEnabled()) {
+         return Uni.createFrom().nullItem();
+      }
+      return searchBackend.remove(indexName, documentId);
+   }
 
    public Multi<String> query(String query) {
       Uni<SearchResult> result = searchBackend.query(query);
--- manager/src/main/java/io/gingersnapproject/search/SearchBackend.java
+++ manager/src/main/java/io/gingersnapproject/search/SearchBackend.java
@@ -6,8 +6,6 @@ public interface SearchBackend {
 
    String PROPERTY = "service.index";
 
-   Uni<String> mapping(String indexName);
-
    Uni<String> put(String indexName, String documentId, String jsonString);
 
    Uni<String> remove(String indexName, String documentId);
