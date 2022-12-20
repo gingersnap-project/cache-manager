2621090617111859a12a7d421d9283fe6815e99a
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-12-20 14:08:57 +0100

    Integrate bulk indexing with put all operation

--- manager/src/main/java/io/gingersnapproject/search/IndexingHandler.java
+++ manager/src/main/java/io/gingersnapproject/search/IndexingHandler.java
@@ -1,5 +1,7 @@
 package io.gingersnapproject.search;
 
+import java.util.Map;
+
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -23,6 +25,14 @@ public class IndexingHandler {
          return Uni.createFrom().nullItem();
       }
       return searchBackend.get().put(indexName, documentId, jsonString);
+   }
+
+   public Uni<String> putAll(String indexName, Map<String, String> documents) {
+      Rule rule = configuration.rules().get(indexName);
+      if (rule == null || !rule.queryEnabled()) {
+         return Uni.createFrom().nullItem();
+      }
+      return searchBackend.get().putAll(indexName, documents);
    }
 
    public Uni<String> remove(String indexName, String documentId) {
