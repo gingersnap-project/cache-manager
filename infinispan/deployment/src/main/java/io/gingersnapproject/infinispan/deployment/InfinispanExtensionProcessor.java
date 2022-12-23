7f5d3df3b2934ab2d5a263769053a982ee96f1e1
jabolina <jbolina@redhat.com>
2022-12-23 16:21:07 -0300

    Adding caffeine classes

--- infinispan/deployment/src/main/java/io/gingersnapproject/infinispan/deployment/InfinispanExtensionProcessor.java
+++ infinispan/deployment/src/main/java/io/gingersnapproject/infinispan/deployment/InfinispanExtensionProcessor.java
@@ -6,7 +6,9 @@ import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 class InfinispanExtensionProcessor {
     @BuildStep
-    void addReflection(BuildProducer<ReflectiveClassBuildItem> reflectionClass) {
+    void addReflection(BuildProducer<ReflectiveClassBuildItem> builder) {
         // TODO: add as we need reflection moving forward
+        builder.produce(new ReflectiveClassBuildItem(false, false, "com.github.benmanes.caffeine.cache.SSMW"));
+        builder.produce(new ReflectiveClassBuildItem(false, false, "com.github.benmanes.caffeine.cache.PSMW"));
     }
 }
